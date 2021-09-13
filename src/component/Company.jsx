168a42774f3769549c3a953a35ef4321e005f3dc
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-13 15:03:08 +0200

    company

--- src/component/Company.jsx
+++ src/component/Company.jsx
@@ -1,20 +1,19 @@
 import { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 
-class SingleJob extends Component {
+class Company extends Component {
   state = {};
 
   render() {
-    const { job } = this.props;
     return (
-      <Card className="my-3">
+      <Card className="mt-3">
         <Card.Body>
-          <Card.Title>{job.title}</Card.Title>
-          <Card.Text>{job.company_name}</Card.Text>
+          <Card.Title>job.title</Card.Title>
+          <Card.Text>job.company_name</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
     );
   }
 }
-export default SingleJob;
+export default Company;
--- src/component/Jobs.jsx
+++ src/component/Jobs.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Company from "./Company";
 import SingleJob from "./SingleJob";
 
 class Jobs extends Component {
@@ -12,6 +13,7 @@ class Jobs extends Component {
       const response = await fetch("https://remotive.io/api/remote-jobs");
       if (response.ok) {
         const res = await response.json();
+        console.log(res.jobs);
         this.setState({
           jobs: res.jobs,
         });
@@ -32,7 +34,9 @@ class Jobs extends Component {
               .map((job) => <SingleJob key={job.id} job={job} />)
               .slice(0, 10)}
           </Col>
-          <Col sm={7}>Supp</Col>
+          <Col sm={7}>
+            <Company />
+          </Col>
         </Row>
       </Container>
     );
