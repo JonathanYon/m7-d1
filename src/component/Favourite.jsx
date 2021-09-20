58ea15fdbe455063bb6d9dc46471ab6f8065da86
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-20 16:42:56 +0200

    oneJob changed to func comp

--- src/component/Favourite.jsx
+++ src/component/Favourite.jsx
@@ -1,46 +1,53 @@
 import { Component } from "react";
 import { Button, Container, Row } from "react-bootstrap";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import OneJob from "./OneJob";
 
-const mapStateToProps = (state) => ({
-  companies: state.favorite.companies,
-});
+// const mapStateToProps = (state) => ({
+//   companies: state.favorite.companies,
+// });
 
-const mapDispatchToProps = (dispatch) => ({
-  removeCompany: (company) =>
-    dispatch({
-      type: "REMOVE_COMPANY",
-      payload: company,
-    }),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   removeCompany: (company) =>
+//     dispatch({
+//       type: "REMOVE_COMPANY",
+//       payload: company,
+//     }),
+// });
 
-class Favorite extends Component {
-  render() {
-    const { companies, removeCompany } = this.props;
+// class Favorite extends Component {
+const Favorite = ({ removeCompany, history }) => {
+  // render() {
+  // const { companies, removeCompany } = this.props;
 
-    return (
-      <>
-        <Container>
-          <Button onClick={() => this.props.history.push("/")}>Back</Button>
-          <h4>My Favourite jobs</h4>
-          <Row>
-            {companies.map((company, i) => (
-              <div className="d-flex flex-column" key={`key ${i}`}>
-                <OneJob job={company} index={i} remove={removeCompany} />
-                {/* <Button
+  const companies = useSelector((state) => state.favorite.companies);
+
+  return (
+    <>
+      <Container>
+        {/* <Button onClick={() => this.props.history.push("/")}>Back</Button> */}
+        <Button onClick={() => history.push("/")}>Back</Button>
+        <h4>My Favourite jobs</h4>
+        <Row>
+          {companies.map((company, i) => (
+            <div className="d-flex flex-column" key={`key ${i}`}>
+              {/* <OneJob job={company} index={i} remove={removeCompany} /> */}
+              <OneJob job={company} index={i} />
+              {/* <Button
                   className="w-25"
                   onClick={() => removeCompany(i)}
                   //   onClick={() => removeCompany(company.title)} //removing based on the name of the job but its not efficient
                 >
                   Remove
                 </Button> */}
-              </div>
-            ))}
-          </Row>
-        </Container>
-      </>
-    );
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
+            </div>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+  // }
+};
+// export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
+export default Favorite;
--- src/component/OneJob.jsx
+++ src/component/OneJob.jsx
@@ -1,43 +1,55 @@
 import { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 import { withRouter } from "react-router";
+import { useDispatch } from "react-redux";
 
-class OneJob extends Component {
-  render() {
-    console.log("OneJob props", this.props);
-    console.log("from Location", this.props.location.pathname);
-    console.log("from match", this.props.match.path);
+// {
+//   type: "REMOVE_COMPANY",
+//   payload: company,
+// }
 
-    const { job, index, remove, location } = this.props;
-    // const fav = location.pathname
-    console.log("props", this.props);
-    return (
-      <Card className=" mr-3 my-3">
-        <Card.Body>
-          <Card.Title>{job.title}</Card.Title>
-          <Card.Text>Company Name: {job.company_name}</Card.Text>
-          <Card.Text>Location: {job.candidate_required_location}</Card.Text>
-          <Card.Text>
-            Salary Range: {job.salary ? job.salary : "As company salary policy"}
-          </Card.Text>
-          <Card.Text>Job Type: {job.job_type}</Card.Text>
-          <Button variant="primary" onClick={() => job.url}>
-            Apply Here
+// class OneJob extends Component {
+const OneJob = ({ job, index, remove, location }) => {
+  const dispatch = useDispatch();
+
+  // render() {
+
+  // const { job, index, remove, location } = this.props;
+  // const fav = location.pathname
+  // console.log("props", this.props);
+  return (
+    <Card className=" mr-3 my-3">
+      <Card.Body>
+        <Card.Title>{job.title}</Card.Title>
+        <Card.Text>Company Name: {job.company_name}</Card.Text>
+        <Card.Text>Location: {job.candidate_required_location}</Card.Text>
+        <Card.Text>
+          Salary Range: {job.salary ? job.salary : "As company salary policy"}
+        </Card.Text>
+        <Card.Text>Job Type: {job.job_type}</Card.Text>
+        <Button variant="primary" onClick={() => job.url}>
+          Apply Here
+        </Button>
+        {location.pathname === "/favourite" ? (
+          <Button
+            className="w-25"
+            // onClick={() => remove(index)}
+            onClick={() =>
+              dispatch({
+                type: "REMOVE_COMPANY",
+                payload: index,
+              })
+            }
+            //   onClick={() => removeCompany(company.title)} //removing based on the name of the job but its not efficient
+          >
+            Remove
           </Button>
-          {location.pathname === "/favourite" ? (
-            <Button
-              className="w-25"
-              onClick={() => remove(index)}
-              //   onClick={() => removeCompany(company.title)} //removing based on the name of the job but its not efficient
-            >
-              Remove
-            </Button>
-          ) : (
-            <></>
-          )}
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+        ) : (
+          <></>
+        )}
+      </Card.Body>
+    </Card>
+  );
+  // }
+};
 export default withRouter(OneJob);
