012ad415a289751351217cbd0f30d08c876a2f02
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-20 15:55:45 +0200

    useSelector()

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,33 +1,42 @@
 import { Component } from "react";
 import { Button, FormControl, InputGroup } from "react-bootstrap";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-const mapStateToProps = (state) => state;
+// const mapStateToProps = (state) => state;
 
-class Home extends Component {
-  render() {
-    return (
-      <>
-        <h2>Job Search</h2>
-        <div className="d-flex">
-          <InputGroup className="mb-3 w-50 mx-auto">
-            <FormControl
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-              value={this.props.search}
-              onChange={(e) =>
-                this.props.setSearch(e.target.value.toLowerCase())
-              }
-            />
-          </InputGroup>
-          <Link to="/favourite">
-            <Button>{this.props.favorite.companies.length}</Button>
-          </Link>
-        </div>
-      </>
-    );
-  }
-}
-export default connect(mapStateToProps)(Home);
+// class Home extends Component {
+const Home = ({ setSearch, search }) => {
+  // render() {
+  const totalCompanies = useSelector(
+    (state) => state.favorite.companies.length
+  );
+  return (
+    <>
+      <h2>Job Search</h2>
+      <div className="d-flex">
+        <InputGroup className="mb-3 w-50 mx-auto">
+          <FormControl
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            // value={this.props.search}
+            value={search}
+            onChange={(e) =>
+              // this.props.setSearch(e.target.value.toLowerCase())
+              setSearch(e.target.value.toLowerCase())
+            }
+          />
+        </InputGroup>
+        <Link to="/favourite">
+          {/* <Button>{this.props.favorite.companies.length}</Button> */}
+          <Button>{totalCompanies}</Button>
+        </Link>
+      </div>
+    </>
+  );
+  // }
+};
+// export default connect(mapStateToProps)(Home);
+export default Home;
