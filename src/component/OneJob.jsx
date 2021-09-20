4c612cd25f8a3ffcbc05a3662f62f137d276dc06
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-20 15:24:11 +0200

    remove remove from company

--- src/component/OneJob.jsx
+++ src/component/OneJob.jsx
@@ -1,9 +1,15 @@
 import { Component } from "react";
 import { Card, Button } from "react-bootstrap";
+import { withRouter } from "react-router";
 
 class OneJob extends Component {
   render() {
-    const { job, index, remove } = this.props;
+    console.log("OneJob props", this.props);
+    console.log("from Location", this.props.location.pathname);
+    console.log("from match", this.props.match.path);
+
+    const { job, index, remove, location } = this.props;
+    // const fav = location.pathname
     console.log("props", this.props);
     return (
       <Card className=" mr-3 my-3">
@@ -18,16 +24,20 @@ class OneJob extends Component {
           <Button variant="primary" onClick={() => job.url}>
             Apply Here
           </Button>
-          <Button
-            className="w-25"
-            onClick={() => remove(index)}
-            //   onClick={() => removeCompany(company.title)} //removing based on the name of the job but its not efficient
-          >
-            Remove
-          </Button>
+          {location.pathname === "/favourite" ? (
+            <Button
+              className="w-25"
+              onClick={() => remove(index)}
+              //   onClick={() => removeCompany(company.title)} //removing based on the name of the job but its not efficient
+            >
+              Remove
+            </Button>
+          ) : (
+            <></>
+          )}
         </Card.Body>
       </Card>
     );
   }
 }
-export default OneJob;
+export default withRouter(OneJob);
